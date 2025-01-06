64563ac8f1c7b74f645f2bbfa266c4604ff9ff45
SzymonRogozinski <pokeszymon.pl@gmail.com>
2025-01-06 18:41:30 +0100

    q

--- src/model/roller.ts
+++ src/model/roller.ts
@@ -1,4 +1,22 @@
+import data from "../apikey.json"
+
+const rollerState={
+    apiKey:data.api_key,
+    requestUsed: data.api_key!==null && data.api_key.length===36
+};
+
 
 export default function RealRandom(max){
+    if (rollerState.requestUsed){
+        return RealRandomNumber(max);
+    }else
+        return PseudoRandomNumber(max);
+}
+
+function RealRandomNumber(max:number){
+
+}
+
+function PseudoRandomNumber(max:number){
     return Math.floor(Math.random() * (max));
 }
